61769eaf9dd91e066d5a66d9c42a3765036dccb3
Lisa Gordeeva <ligrade@mail.ru>
2020-02-06 00:29:49 +0300

    Update testing.cpp
    Added methods for checking the size  and operator[].

--- testing.cpp
+++ testing.cpp
@@ -5,7 +5,27 @@ class TestPrimes {
 public:
 	Primes num;
 	uint32_t count_test;
+	void SIZE_TEST(uint32_t expected){
+		uint32_t size = num.size();
+		if(size == expected){
+			std::cout << "Test passed." << std::endl;
+			count_test++;
+		}
+		else{
+			std::cout << "Test don't passed." << std::endl;
+		}
+	}
 
+	void OPERATOR_TEST(uint32_t index, uint32_t expected){
+		if (num[index] == expected){
+			std::cout << "Test passed." << std::endl;
+			count_test++;
+		}
+		else{
+			std::cout << "Test don't passed." << std::endl;
+		}
+	}
+	
 }
 
 int main(){
